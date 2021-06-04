7da2f7b42982fb71793be70fc6aa5197cc131c9a
sonthanh1408 <son.nt.485@aptechlearning.edu.vn>
2021-06-04 10:15:11 +0700

    first commit

---
+++ Calligraphy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Calligraphy.Startup))]
+namespace Calligraphy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
